c03751447792844e8cc9300137666b72e1af7f7d
dasJulow <juluswolf@gmailcom>
2022-12-22 17:12:36 +0000

    New

--- background.js
+++ background.js
@@ -1,8 +1,27 @@
-chrome.webRequest.onBeforeReques.addListener(
-    function(details){
-        //Code to redirect request
-        return{redirectUrl: "http://youtube.com"};
+chrome.declarativeNetRequest.addRule({
+    id: 'redirect-to-youtube',
+    priority: 100,
+    condition: {
+      urlFilter: '*://amazon.com/*'
     },
-    {urls: ["*://*.amazon.com/*"]},
-    ["blocking"]
-);
+    action: {
+      type: 'redirect',
+      redirectUrl: 'https://www.youtube.com/'
+    }
+  }, function() {
+    if (chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError.message);
+      return;
+    }
+  
+    // Update the dynamic rules to apply the new rule to all active tabs
+    chrome.declarativeNetRequest.updateDynamicRules(function() {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        return;
+      }
+  
+      console.log('Successfully added declarative rule');
+    });
+  });
+  
